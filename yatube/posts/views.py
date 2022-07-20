c8644870b08e829a08bb6008cfe778afad038ae3
Saggitel <lazba.timur@gmail.com>
2022-07-20 18:50:45 +0300

    Первый коммит

---
+++ yatube/posts/views.py
@@ -0,0 +1,20 @@
+from turtle import title
+from django.shortcuts import render, get_object_or_404
+from .models import Post, Group
+from django.http import HttpResponse
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    context = {
+        'posts': posts,
+    }
+    return render(request, 'posts/index.html', context) 
+
+def group_posts(request, slug):
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, template, context)
